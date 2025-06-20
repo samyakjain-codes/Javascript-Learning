3c541c2ef616fc17f93b6870628e2972b03960bc
Samyak Jain <samyakjain1951@gmail.com>
2025-06-20 23:54:32 +0530

    Initial commit with JavaScript practice files

---
+++ 1 - Variables/script.js
@@ -0,0 +1,17 @@
+// console.log("Hello this is samyak");
+
+const fileId = 12344    // const values cannot be changed once declared
+let fileType = "word"
+var fileName = "Samyak" //avoid
+fileMemory = "2MB"
+
+let fileLocation;
+
+/*
+do not use var
+var does not have proper functional scope
+*/
+
+//console.log(fileId);
+console.table([fileId, fileType, fileName, fileMemory, fileLocation]);
+
---
+++ 2 - Operation Conversion/script.js
@@ -0,0 +1,31 @@
+// title = Operation conversion
+
+let score = "77";
+
+let scoreNumber = String(score); // for conversion write the first letter of datatype in uppercase
+
+// console.log(typeof score); typeof is used when we have to find the datatype of a varaiable or entity
+// console.log(scoreNumber);
+// console.log(typeof scoreNumber);
+
+
+let string = true;
+
+let stringNumber = Number(string);
+
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// "Samyak" => NaN(Not a number); "true" => 1
+
+let age = ""
+
+let booleanAge = Boolean(age);
+
+console.log(booleanAge);
+console.log(typeof booleanAge);
+
+// "" => false ; "18" => true
+
+
