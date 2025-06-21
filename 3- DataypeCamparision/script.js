e07a8d3c07ad15c356ddfe21b9a061b34b5e19b7
Samyak Jain <samyakjain1951@gmail.com>
2025-06-22 00:26:10 +0530

    Added 3 - DatatypeCamparison with script.js

---
+++ 3- DataypeCamparision/script.js
@@ -0,0 +1,30 @@
+  //**************Datatype comparision **************/
+
+     // SAME DATATYPES
+//   console.log(2>1);
+//   console.log(2==1);
+//   console.log(2 - 1);
+//   console.log(2 % 1);
+  
+      // DIFEERENT Datatypes
+// console.log("2" > 1);
+// console.log("2" == 1);
+// console.log("1" >= 2);
+
+// console.log("2" + 1 + 1);
+// console.log(1 + 1 + "1" + 1);
+// console.log(2 + 1 + "1");
+
+   // !!!!!!!!AVOID THIS!!!!!!!!
+//  console.log(null > 0);
+//  console.log(null == 0); 
+//  console.log(null >= 0);
+
+console.log(undefined > 0);
+console.log(undefined >= 0);
+console.log(undefined == 0);
+
+
+
+
+
