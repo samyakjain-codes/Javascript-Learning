082fd13fa18ea3efd894c08c50367dc439b84a46
Samyak Jain <samyakjain1951@gmail.com>
2025-06-22 01:01:34 +0530

    Updated script.js in Variables folder

--- 1 - Variables/script.js
+++ 1 - Variables/script.js
@@ -15,3 +15,32 @@ var does not have proper functional scope
 //console.log(fileId);
 console.table([fileId, fileType, fileName, fileMemory, fileLocation]);
 
+
+
+//******************MEMORY IN JS **********************************8*/
+
+// Stack(Primitive) and Heap (Non-Primitive)
+
+let caller1 = "Robert"
+let caller2 = caller1
+caller2 = "Nolan"
+
+console.log(caller1);
+console.log(caller2);
+
+let user1 = {
+    id : 12345 , 
+    username : "user1@google"
+}
+
+let user2 = user1
+
+user1.id = 2121
+user1.username = "user2@yahoo"
+
+console.log(user1.id);
+console.log(user1.username);
+console.log(user2.id);
+console.log(user2.username);
+
+
